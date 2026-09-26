a6c6e3d49a8badaaa78d4340df52715252577751
DraconDev <dracsharp@gmail.com>
2026-09-26 03:52:33 +0100

    1 file(s) in pi-pro-ask-question-image-draft/src/image-generator.ts DELTA:+1/-1 | EXT:ts

--- pi-pro-ask-question-image-draft/src/image-generator.ts
+++ pi-pro-ask-question-image-draft/src/image-generator.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from "node:crypto";
-import { mkdir, writeFile } from "node:fs/promises";
+import { mkdir, readFile, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 
 import { getAgentDir, readStoredCredential } from "@earendil-works/pi-coding-agent";
